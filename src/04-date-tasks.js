9a107d7d37de04d056bbb0d99e1c7883ecce8661
Vladislav <wlladisla@gmail.com>
2022-12-24 12:27:10 +0300

    feat:add some funcs

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -71,8 +71,9 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const res = new Date(endDate.getTime() - startDate.getTime());
+  return res.toISOString().slice(11, -1);
 }
 
 /**
